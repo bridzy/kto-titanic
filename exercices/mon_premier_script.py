7bbdf90542b18c56f714269356fea52d0a329d1f
Ouail_LEKHCHINE <olekhchine@gmail.com>
2026-02-16 14:02:55 +0000

    refactor : clean & principes fondamentaux pour mon premier script

--- exercices/mon_premier_script.py
+++ exercices/mon_premier_script.py
@@ -1,3 +1,5 @@
+import unittest
+
 message = "C'est mon premier script !!!"
 print(message)
 
@@ -6,17 +8,38 @@ print(type(je_change_de_type))
 je_change_de_type = "coucou"
 print(type(je_change_de_type))
 
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-more_than_seven = 0
-for prenom in prenoms:
-    if len(prenom) > 7:
-        more_than_seven += 1
-        print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-    else:
-        print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(more_than_seven))
+
+"""
+Count names with more than seven letters
+"""
+def names(prenoms: list[str]) -> int:
+    more_than_seven = 0
+
+    for prenom in prenoms:
+        if len(prenom) > 7:
+            more_than_seven += 1
+            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+        else:
+            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+
+    return more_than_seven
+
 
 def saluer(nom: str) -> str:
     return "Bonjour " + nom
 
-print(saluer("Alice"))  # Affiche : Bonjour Alice
+
+class TestNamesMethod(unittest.TestCase):
+    def test_names(self) -> None:
+        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        more_than_seven = names(prenoms=prenoms)
+        self.assertEqual(more_than_seven, 4)
+
+
+if __name__ == "__main__":
+    prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+    print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
+
+    print(saluer("Alice"))  # Affiche : Bonjour Alice
+
+    unittest.main()

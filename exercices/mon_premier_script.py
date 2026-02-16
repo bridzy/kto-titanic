bb9fd199642c966b777bf9053afad8821025ccaf
Ouail_LEKHCHINE <olekhchine@gmail.com>
2026-02-16 15:02:44 +0000

    refactor : clean & principes fondamentaux du py  pour mon premier script v2

--- exercices/mon_premier_script.py
+++ exercices/mon_premier_script.py
@@ -9,10 +9,8 @@ je_change_de_type = "coucou"
 print(type(je_change_de_type))
 
 
-"""
-Count names with more than seven letters
-"""
 def names(prenoms: list[str]) -> int:
+    """Compter les prénoms qui ont plus de 7 lettres (et afficher le message associé)."""
     more_than_seven = 0
 
     for prenom in prenoms:
@@ -25,21 +23,22 @@ def names(prenoms: list[str]) -> int:
     return more_than_seven
 
 
+prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+more_than_seven = names(prenoms)
+print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(more_than_seven))
+print("----------------------------------------------------------------------")
+
+
 def saluer(nom: str) -> str:
     return "Bonjour " + nom
+#print(saluer("Alice"))  # Affiche : Bonjour Alice
 
 
 class TestNamesMethod(unittest.TestCase):
     def test_names(self) -> None:
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+        prenoms_test = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        self.assertEqual(names(prenoms_test), 4)
 
 
 if __name__ == "__main__":
-    prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-    print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
-
-    print(saluer("Alice"))  # Affiche : Bonjour Alice
-
-    unittest.main()
+    unittest.main() 
